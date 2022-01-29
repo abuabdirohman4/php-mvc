851d57435832b17268ad30fd3c9b0b5be5098d6d
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-01-29 19:34:40 +0700

    create .htaccess

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,15 @@
 class App {
     public function __construct()
     {
-        echo 'Hello World';
+        // var_dump($_GET);
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL() {
+        if ( isset($_GET['url']) ) {
+            $url = $_GET['url'];
+            return $url;
+        }
     }
 }
