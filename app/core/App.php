f9635e835c745b3e9ecf703ed3f5ba77be10c329
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-02-05 06:27:41 +0700

    #2 routing

--- app/core/App.php
+++ app/core/App.php
@@ -5,12 +5,16 @@ class App {
     {
         // var_dump($_GET);
         $url = $this->parseURL();
+        echo "hello <br>";
         var_dump($url);
     }
 
     public function parseURL() {
         if ( isset($_GET['url']) ) {
-            $url = $_GET['url'];
+            // $url = $_GET['url'];
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
             return $url;
         }
     }
