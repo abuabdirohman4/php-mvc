c3b774db27310b02f9ccd07c12ada0407772440a
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-02-07 21:26:04 +0700

    update controller

--- app/core/App.php
+++ app/core/App.php
@@ -1,12 +1,20 @@
 <?php
 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'indes';
+    protected $params = [];
+
     public function __construct()
     {
         // var_dump($_GET);
         $url = $this->parseURL();
-        echo "hello <br>";
-        var_dump($url);
+        // echo "hello <br>";
+        // var_dump($url);
+        if (file_exists('../controllers/' . $url[0]) . '.php') {
+            $this->controller = $url[0];
+            var_dump($url);
+        }
     }
 
     public function parseURL() {
