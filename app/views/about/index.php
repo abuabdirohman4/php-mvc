f8eff0c80b15a72f56115940705c8a2240c1991a
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-03-25 21:26:31 +0700

    #5 assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,5 @@
 <div class="container">
-    <h1>About Me</h1>
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL ?>/img/profile.jpg" width="200" class="rounded-circle shadow mt-2 mb-4" alt="Abu Abdirohman">
     <p>Halo, nama saya <?= $data['nama'] ?>, saya adalah seorang <?= $data['pekerjaan'] ?>, Saya berumur <?= $data['umur'] ?> tahun</p>
 </div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,6 @@
-<h1>Selamat Datang di Web MVC</h1>
+<div class="container">
+    <div class="mt-4 p-5 bg-secondary text-white rounded">
+        <h1>Selamat Datang di PHP MVC</h1>
+        <p>Lorem ipsum...</p>
+    </div>
+</div>
